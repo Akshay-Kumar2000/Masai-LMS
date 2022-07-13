10dd52168892291d143a436df6c2b6161e220f90
Akshay Kumar <93372936+Akshay-Kumar2000@users.noreply.github.com>
2022-07-13 23:47:53 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import "./App.css";
-
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
     <h1>Let's start with the Masai LMS</h1>
+    <Login />
     </>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Form, Alert } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import GoogleButton from "react-google-button";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  return (
+    <>
+      <div className="p-4 box">
+        <h2 className="mb-3">Firebase Auth Login</h2>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <Form >
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <div className="d-grid gap-2">
+            <Button variant="primary" type="Submit">
+              Log In
+            </Button>
+          </div>
+        </Form>
+        <hr />
+        <div>
+          <GoogleButton
+            className="g-btn"
+            type="dark"
+          />
+        </div>
+      </div>
+      <div className="p-4 box mt-3 text-center">
+        Don't have an account? 
+      </div>
+    </>
+  );
+};
+
+export default Login;
