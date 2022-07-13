4cc6f04b8437fe3c321884a509fcba95c9bcda48
Akshay Kumar <93372936+Akshay-Kumar2000@users.noreply.github.com>
2022-07-14 00:48:32 +0530

    signup page done

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { useUserAuth } from "../context/UserAuthContext";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
   const [password, setPassword] = useState("");
+  const { signUp } = useUserAuth();
+  let navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signUp(email, password);
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 
   return (
     <>
       <div className="p-4 box">
         <h2 className="mb-3">Firebase Auth Signup</h2>
         {error && <Alert variant="danger">{error}</Alert>}
-        <Form >
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Control
               type="email"
@@ -25,6 +40,7 @@ const Signup = () => {
             <Form.Control
               type="password"
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
 
@@ -36,7 +52,7 @@ const Signup = () => {
         </Form>
       </div>
       <div className="p-4 box mt-3 text-center">
-        Already have an account?
+        Already have an account? <Link to="/">Log In</Link>
       </div>
     </>
   );
